4e5eeaf0721198b3899618eac026fd35f3ae138f
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-05-10 17:03:33 +0530

    feat: add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <ChakraProvider>
       <Component {...pageProps} />
-    </div>
+    </ChakraProvider>
   );
 }
 
