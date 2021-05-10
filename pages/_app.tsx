efdd35cb535707ecfc2351d54d8314af13b6ea3e
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-05-10 21:21:09 +0530

    feat: updated font styles and assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,27 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "../styles/globals.css";
+import "../styles/fonts.css";
+
+const customTheme = extendTheme({
+  colors: {
+    white: {
+      100: "#E0E0E0",
+      500: "#F9F9F9",
+      900: "#fff",
+    },
+    black: {
+      500: "#656464",
+      900: "#000",
+    },
+    red: {
+      900: "#FF0000",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
