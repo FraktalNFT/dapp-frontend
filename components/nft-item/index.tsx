3731d3b6daa73d8c40e88a0cb66a8a902c269446
Nicholas Patrick Otto Caputo <npocaputo@gmail.com>
2021-10-12 11:35:19 -0700

    Add drop shadow and hover

--- components/nft-item/index.tsx
+++ components/nft-item/index.tsx
@@ -16,15 +16,20 @@ const NFTItem = forwardRef<HTMLDivElement, NFTItemProps>(
     return (
       <VStack
         cursor='pointer'
-        overflow='hidden'
         maxW='30rem'
         rounded='md'
         borderWidth='1px'
-        borderColor='white.100'
+        boxShadow="md"
         onClick={onClick}
+        _hover={{
+          boxShadow: "xl"
+        }}
         ref={ref}
       >
-        <Box h='35rem' w='100%' position='relative' >
+        <Box 
+          h='35rem' 
+          w='100%' 
+          position='relative' >
           <Image src={item.imageURL} width='100%' height='100%' objectFit='cover' margin-left='auto' margin-right='auto' display='flex' maxH='35rem' style={{verticalAlign:'middle'}}/>
           {item.countdown && (
             <VStack
