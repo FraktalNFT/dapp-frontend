386c12513395cabccd1f2f58b61d78d52cc5fb4d
Mackenzie Bowes <mackenziepbowes@gmail.com>
2021-11-18 15:05:57 -0800

    Import Page (#33)
    * modified addresses and check for a new user
    
    * new contracts, new subgraph, corrected fraktions holders, corrected list items button conditions
    
    * modified addresses and check for a new user
    
    * starting new mint & list page
    
    * added extra button for states and actions
    
    * added components for mint and list inputs
    
    * added user context to handle wallet assets
    
    * cleaned up my-nfts and added grid of selection on mintPage
    
    * cleaning
    
    * added user context and import funcitons
    
    * solved bug for new users, fixed error in subgraph for erc721unlocked events
    
    * front end for import nft
    
    * resolved merge conflicts again
    
    Co-authored-by: drhongos <lucasziegenfuhs@gmail.com>

---
+++ components/nft-importcard-opensea/index.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@chakra-ui/button";
+import { Image } from "@chakra-ui/image";
+import { Box, Center, StackProps, Text, VStack } from "@chakra-ui/layout";
+import { formatEther } from "@ethersproject/units";
+import React, { forwardRef } from "react";
+import { FrakCard } from "../../types";
+import FrakButton from "../button";
+import NextLink from "next/link";
+import {useState} from 'react';
+import { useWeb3Context } from '../../contexts/Web3Context';
+import {approveMarket, importERC721, importERC1155} from '../../utils/contractCalls';
+import { claimNFT } from '../../utils/helpers';
+
+interface NFTItemProps extends StackProps {
+  item: FrakCard;
+  CTAText?: string;
+  onCollateralRequest?: void;
+}
+
+const NFTImportCardOS = forwardRef<HTMLDivElement, NFTItemProps>(
+  ({ item, onClick, CTAText, onCollateralRequest }, ref) => {
+
+    return (
+      <VStack
+        cursor='pointer'
+        overflow='hidden'
+        maxW='30rem'
+        rounded='md'
+        borderWidth='1px'
+        borderColor='white.100'
+        onClick={onClick}
+        ref={ref}
+      >
+        <Box minH='30rem' w='100%' position='relative' >
+          <Image src={item.imageURL} width='100%' height='100%' style={{verticalAlign:'auto'}}/>
+        </Box>
+          <Text mb='1.6rem' sx={{width: `100%`, padding: `0 1rem`, fontFamily: `Inter, sans-serif`, fontWeight: `700`}}>
+            {item?.name}
+          </Text>
+      </VStack>
+    );
+  }
+);
+
+export default NFTImportCardOS;
