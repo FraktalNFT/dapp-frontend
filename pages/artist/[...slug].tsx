2989fd0726ccbc6afba4a609efacb62f22cb40cd
Ervin Dimitri <ervinluis84@gmail.com>
2022-02-03 04:26:48 +0200

    Added SearchBar for Artist Page

--- pages/artist/[...slug].tsx
+++ pages/artist/[...slug].tsx
@@ -2,6 +2,7 @@ import { Grid, Text, VStack, Heading, Spinner, Box } from "@chakra-ui/react";
 import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import styles from "./artist.module.css";
+import styles_Search from "../../styles/artists.module.css";
 import NFTItem from "../../components/nft-item";
 import FrakButton from "../../components/button";
 import NextLink from "next/link";
@@ -10,12 +11,21 @@ import { getSubgraphData } from "../../utils/graphQueries";
 import { createObject2 } from "../../utils/nftHelpers";
 import toast from "react-hot-toast";
 import { useRouter } from "next/router";
+import { Image } from "@chakra-ui/image";
 
 export default function ArtistView() {
   const router = useRouter();
   const [artistAddress, setArtistAddres] = useState("");
   const [nftItems, setNftItems] = useState([]);
   const [loading, setIsLoading] = useState<boolean>(false);
+  const [artist_Address, setArtist_Address] = useState("");
+  
+  function searchHandle(e){
+    e.preventDefault();
+    if(e.target.value !== ""){
+      setArtist_Address(e.target.value);
+    }
+  }
 
   useEffect(() => {
     async function getData() {
@@ -43,7 +53,7 @@ export default function ArtistView() {
       }
     }
     getData();
-  }, []);
+  }, [router.asPath]);
 
   return (
     <VStack spacing="0" mb="1rem" sx={{ alignItems: `start` }}>
@@ -52,6 +62,23 @@ export default function ArtistView() {
       </Head>
       <Heading sx={{ fontSize: `36px`, marginBottom: `1rem` }}>
         {artistAddress}
+        <div className={styles_Search.searchContainer}>
+          <input
+            placeholder={"Search ENS or ETH address "}
+            className={styles_Search.searchInput}
+            onChange={searchHandle}
+          />
+          {
+            artist_Address !=="" &&
+              <NextLink
+                      href={`/artist/${artist_Address}`}
+                      key={`link--${artist_Address}`}
+                    >
+                <Image src="/search.svg" />
+              </NextLink>
+          }
+          
+        </div>
       </Heading>
       {!loading && nftItems.length > 0 && (
         <>
