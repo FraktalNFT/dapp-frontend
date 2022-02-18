e7323721ff04b8b1a52bf8b0abccf16a60e70a0a
AlexSnipes <dev@freakoffice.com>
2022-02-17 21:37:33 -0300

    Anchor Fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -131,14 +131,4 @@ const LandingPage = () => {
     </>);
 };
 
-function Anchor({ href, passHref, target, children }) {
-    return (
-        <NextLink href={href} >
-            <a href={href} >
-                {children}
-            </a>
-        </NextLink>
-    )
-}
-
 export default LandingPage;
