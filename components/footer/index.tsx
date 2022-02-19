7cb7af21b72c2b1b1270a71b94b9d01c34a0a2eb
Grant Steuart <gmsteuart@gmail.com>
2022-02-19 14:10:33 -0600

    style: minor style changes

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -49,7 +49,7 @@ const Footer: React.FC = () => {
       pt={["15rem", "30rem"]}
     >
       <Flex direction={["column", "column", "row", "row"]} justify="space-between">
-        <Box alignItems="start" flex={["1 1", "1 1", "1 1", "0.5 1"]} mr="15rem">
+        <Box alignItems="start" flex={["1 1", "1 1", "1 1", "0.6 1"]}>
           <Image src="/fraktal-full-logo.png" w={["100%", "320px"]}/>
           <Text fontSize={16} my="40px" lineHeight="30px" fontWeight={500} color="grey.500">
             Fraktal is a community first project, with a mission to to empower 
@@ -58,11 +58,10 @@ const Footer: React.FC = () => {
           </Text>
 
           <Box>
-            <Heading textAlign="center" mb="2rem">Follow Us!</Heading>
             <Flex direction="row" justify="space-evenly" gap={16} mb="4rem">
               {socials.map(({url, icon}, idx) => (
                 <Link href={url} isExternal rel="noreferrer noopener" key={`social-${idx}`}>
-                  <Icon as={icon} w="32px" h="32px" />
+                  <Icon as={icon} w="48px" h="48px" />
                 </Link>
               ))}
             </Flex>
