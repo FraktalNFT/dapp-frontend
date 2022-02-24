9f8cba433bf169474be112bdf008a779eecf17be
naftalimurgor <murgornaftali@gmail.com>
2022-02-24 09:19:42 +0300

    feat: WIP-Transfer NFT. Add component `TransferNFT` for page that maps to route `/transfer-nft`

---
+++ pages/transfer-nft.tsx
@@ -0,0 +1,12 @@
+ import {useEffect, useState} from "react";
+ import { useRouter } from "next/router";
+ const TransferNFT = (): JSX.Element => {
+     const router = useRouter(); 
+     return(<>
+       <div>Transfer NFT page</div>
+      </>
+     )
+ };
+ 
+ export default TransferNFT;
+ 
