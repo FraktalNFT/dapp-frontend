99105e15ac052aabac18f63d96e46830c7064e32
naftalimurgor <murgornaftali@gmail.com>
2022-02-24 21:46:43 +0300

    feat: WIP-Transfer NFT. Make `/transfer-nft` a dynamic route

--- pages/nft/[id]/transfer-nft.tsx
+++ pages/nft/[id]/transfer-nft.tsx
@@ -1,7 +1,13 @@
  import {useEffect, useState} from "react";
  import { useRouter } from "next/router";
  const TransferNFT = (): JSX.Element => {
-     const router = useRouter(); 
+     const router = useRouter();
+      // get nft data from the route
+      // nice card with the image of the nft
+      // input of the address
+      // button to confirm transfer
+      // transfer nft
+      // feedback to the UI 
      return(<>
        <div>Transfer NFT page</div>
       </>
