f65ad1fab223c617246298a2354a9308bc9e7aa6
AlexSnipes <alejandro.sanchez.trek@gmail.com>
2022-03-11 19:58:51 -0300

    verifying NFT authenticity - https://github.com/FraktalNFT/dapp-frontend/issues/229

--- components/fraktionsList/index.tsx
+++ components/fraktionsList/index.tsx
@@ -1,87 +1,167 @@
-import React from "react";
-import FraktionsDetail from '../fraktionsDetail';
-import FrakButton from '../../components/button4'
+/**
+ * React
+ */
+import React, {useEffect, useState}  from "react";
+/**
+ * Chakra
+ *
+ */
 import {
+  Link,
+  Text,
   Icon,
-  Tooltip
+  Tooltip,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
 } from '@chakra-ui/react';
+import FraktionsDetail from '../fraktionsDetail';
+/**
+ * Component
+ */
+import FrakButton from '../../components/button4'
+/**
+ * Icons
+ */
 import { AiOutlineInfoCircle } from 'react-icons/ai';
-
+import {validateAsset} from "@/utils/openSeaAPI";
 // if account has fraktions.. display info to list?
 
-const FraktionsList=(({fraktionsListed, tokenAddress, marketAddress, provider}) => {
+const FraktionsList=(({nftObject, fraktionsListed, tokenAddress, marketAddress, provider}) => {
 
+  const [opeaSeaURL, setOpeaSeaURL] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [message, setMessage] = useState("");
+  const [validationState, setValidationState] = useState(false);
+
+  async function validateNFT() {
+    setValidationState(true);
+    const response = await validateAsset(nftObject.collateral.id, 1);
+    setValidationState(false);
+    if (response.success === false) {
+        setMessage("NFT is not valid");
+        setOpenModal(true);
+    } else if (response.permalink !== undefined) {
+        setOpeaSeaURL(response.permalink);
+        window.open(response.permalink, '_blank');
+    }
+  }
 
   return(
-    <div style={{
-      borderRadius:'4px',
-      borderWidth:'1px',
-      borderColor:'#E0E0E0',
-      padding: '16px',
-      marginTop:'40px 0px'
-    }}
-    >
-    <div style={{
-      color:'#5A32F3',
-      fontWeight:'bold',
-      fontFamily:'Inter',
-      fontSize:'24px',
-      lineHeight:'29px',
-      display: "flex",
-      justifyContent: "space-between"
-    }}>
-      Fraktions
-      <div>
-      <FrakButton
-        onClick={() =>
-          console.log('Button click')
-        }
-      >
-        Verify On Opensea
-      </FrakButton>
-      <Tooltip
-        border=" 1px solid #00C49D"
-        borderRadius="4px"
-        boxShadow="none"
-        padding="8px"
-        fontSize="14px"
-        bg="#fff"
-        color="#656464"
-        placement="top"
-        label="Make sure to check that the NFT you are buying Fraktions of is authentic."
-        offset={[0, 20]}
-      >
+      <>
+        <ModalOpenSeaValidation
+            nftObject={nftObject}
+            message={message}
+            opeaSeaURL={opeaSeaURL}
+            openModal={openModal}
+            setOpenModal={setOpenModal}  />
+        <div style={{
+          borderRadius:'4px',
+          borderWidth:'1px',
+          borderColor:'#E0E0E0',
+          padding: '16px',
+          marginTop:'40px 0px'
+        }}
+        >
+          <div style={{
+            color:'#5A32F3',
+            fontWeight:'bold',
+            fontFamily:'Inter',
+            fontSize:'24px',
+            lineHeight:'29px',
+            display: "flex",
+            justifyContent: "space-between"
+          }}>
+            Fraktions
+            <div>
+              <FrakButton
+                  disabled={validationState}
+                  onClick={() =>
+                      validateNFT()
+                  }
+              >
+                Verify On Opensea
+              </FrakButton>
+              <Tooltip
+                  border=" 1px solid #00C49D"
+                  borderRadius="4px"
+                  boxShadow="none"
+                  padding="8px"
+                  fontSize="14px"
+                  bg="#fff"
+                  color="#656464"
+                  placement="top"
+                  label="Make sure to check that the NFT you are buying Fraktions of is authentic."
+                  offset={[0, 20]}
+              >
         <span style={{ cursor: 'pointer', paddingLeft: 4 }}>
           <Icon as={AiOutlineInfoCircle} w={10} h={10} color="#00C49D" />
         </span>
-      </Tooltip>
-      </div>
-      
-    </div>
-      {fraktionsListed && fraktionsListed.length ?
-        <div>
-          {fraktionsListed.map(x=>{
-            return(
-              <FraktionsDetail
-                key={x.id}
-                amount={x.amount}
-                price={x.price}
-                seller={x.seller.id}
-                tokenAddress={tokenAddress}
-                marketAddress={marketAddress}
-                provider={provider}
-              />
-            )
-          })}
-        </div>
-        :
-        <div style={{marginTop:'24px'}}>
-          There are no listed Fraktions of this NFT.
-        </div>
-      }
-    </div>
+              </Tooltip>
+            </div>
 
+          </div>
+          {fraktionsListed && fraktionsListed.length ?
+              <div>
+                {fraktionsListed.map(x=>{
+                  return(
+                      <FraktionsDetail
+                          key={x.id}
+                          amount={x.amount}
+                          price={x.price}
+                          seller={x.seller.id}
+                          tokenAddress={tokenAddress}
+                          marketAddress={marketAddress}
+                          provider={provider}
+                      />
+                  )
+                })}
+              </div>
+              :
+              <div style={{marginTop:'24px'}}>
+                There are no listed Fraktions of this NFT.
+              </div>
+          }
+        </div>
+     </>
   )
 
 })
+
+const ModalOpenSeaValidation = ({nftObject, opeaSeaURL, openModal, setOpenModal, message}) => {
+
+  const closeModal = () => {
+    setOpenModal(false);
+  }
+
+  const isValidated = () => {
+    if (validationState == "OK") {
+      return true;
+    }
+  }
+
+  return (
+      <Modal isOpen={openModal} onClose={closeModal}>
+        <ModalOverlay />
+        <ModalContent
+            textAlign={'center'}
+            maxWidth={400}
+            padding={'60px 0'}
+            borderRadius={15}
+            boxShadow={'0px 7px 20px rgba(0, 0, 0, 0.35)'}
+        >
+          <ModalHeader>{nftObject.name}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+              <Text color="rgba(255, 0, 0, 1);">{message}</Text>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+  );
+}
+
 export default FraktionsList;
--- utils/openSeaAPI.ts
+++ utils/openSeaAPI.ts
@@ -1,9 +1,8 @@
 const rinkebyBaseURL = 'https://rinkeby-api.opensea.io/api/v1/';
-
 const assetsRequest = 'https://api.opensea.io/api/v1/assets';
-
 const options = {method: 'GET'};
-
+const ASSET_METHOD = 'asset';
+const ASSETS_METHOD = 'assets';
 
 export const assetsInWallet = (address) => {
   const data = fetch(`https://rinkeby-api.opensea.io/api/v1/assets?owner=${address}&order_direction=desc&offset=0&limit=20`, options)
@@ -11,9 +10,18 @@ export const assetsInWallet = (address) => {
     .then(data => {
       return data;
     })
-    // .then(response => console.log(response))
     .catch(err => console.error(err));
   return data;
+}
+
+export const validateAsset = (contract, tokenId) => {
+    const data = fetch(process.env.NEXT_PUBLIC_OPENSEA_API + ASSET_METHOD + `/` + contract + '/' + tokenId, options)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(err => console.error(err));
+    return data;
 }
 
 
